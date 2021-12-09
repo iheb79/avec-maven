e2da7b6add9c6d1e58a96688d972e7a57db475a1
IHEB MELLITI <mellitiiheb191@gmail.com>
2021-12-09 17:20:43 +0100

    2 eme commit

--- src/main/java/tn/esprit/CallRestWebService.java
+++ src/main/java/tn/esprit/CallRestWebService.java
@@ -21,6 +21,7 @@ import org.json.JSONObject;
 
 public class CallRestWebService {
 
+	//new Commit
 	public static final String endpoint = "http://ip-api.com/json";
 	//public static final String endpoint = "https://httpbin.org/get";
 	public static void main(String[] args) {
